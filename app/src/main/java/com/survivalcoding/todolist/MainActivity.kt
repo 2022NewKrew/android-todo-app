d61bfc680ccd3b84889d51776fd2191574a300a9
evan.969 <evan.969@kakaocorp.com>
2021-12-28 15:34:08 +0900

    todolist with baseAdapter

--- app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
@@ -2,11 +2,26 @@ package com.survivalcoding.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.survivalcoding.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        println("hi")
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // sample data list
+        val dataList = (0..30).map { SampleData("title $it") }
+        val todoListAdapter = TodoListAdapter(dataList)
+
+        binding.vTodoList.adapter = todoListAdapter
+
+
     }
+
 }
