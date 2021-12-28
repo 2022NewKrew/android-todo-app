03c57b34e6448ebf08253f78b9c8586067b8e30c
jionchu <fn0809@gmail.com>
2021-12-28 17:09:25 +0900

    floating action button 추가

--- app/src/main/java/com/survivalcoding/todolist/main/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/todolist/main/MainActivity.kt
@@ -2,7 +2,9 @@ package com.survivalcoding.todolist.main
 
 import android.os.Bundle
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.survivalcoding.todolist.R
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
         // 리스트뷰 설정
         val listView = findViewById<ListView>(R.id.main_lv_todo)
         listView.adapter = TodoAdapter(data)
+
+        val fab = findViewById<FloatingActionButton>(R.id.main_fab_add)
+        fab.setOnClickListener {
+            Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
