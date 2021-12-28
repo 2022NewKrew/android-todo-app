2ca751eec0779aa3645ef6efe9e51e4a4378b91d
peter.shlee <peter.shlee@kakaocorp.com>
2021-12-28 15:49:53 +0900

    ListView 이용하여 간단한 숫자 리스트 구현

--- app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
@@ -2,10 +2,14 @@ package com.survivalcoding.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listNumbers = findViewById<ListView>(R.id.list_numbers)
+        listNumbers.adapter = SimpleNumberListAdapter((0..30).toList())
     }
 }
