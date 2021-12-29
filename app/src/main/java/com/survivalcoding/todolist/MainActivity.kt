faf601a2b19e2cc30e557e03cb66fbd66d7a45d3
shy.yi <shy.yi@kakaocorp.com>
2021-12-29 17:10:56 +0900

    Delete Day 2 Codes

--- app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
@@ -3,6 +3,7 @@ package com.survivalcoding.todolist
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.ListView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
@@ -16,14 +17,10 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Result OK", Toast.LENGTH_SHORT).show()
         }
     }
-    private val todos = (0..30).map { "Todo #${it}" }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val listView = findViewById<ListView>(R.id.listView)
-        listView.adapter = ToDoArrayAdapter(todos)
 
         val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener {
