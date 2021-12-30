f7149feca0f18bd136414e773d85a27e692addf8
shy.yi <shy.yi@kakaocorp.com>
2021-12-30 21:47:40 +0900

    Glue Them All Together in MainActivity.kt

--- app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/todolist/MainActivity.kt
@@ -5,10 +5,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.survivalcoding.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val viewModel: MainActivityViewModel by viewModels()
 
     private val pingPongWriteActivity =
         registerForActivityResult(StartActivityForResult()) { result ->
@@ -17,13 +24,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-    private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.todoListRv.adapter =
+            TodoListAdapter {
+                viewModel.toggleTodos(it)
+                submitList(viewModel.todos)
+            }.apply { submitList(viewModel.todos) }
 
         binding.fab.setOnClickListener {
             pingPongWriteActivity.launch(Intent(this, WriteActivity::class.java))
