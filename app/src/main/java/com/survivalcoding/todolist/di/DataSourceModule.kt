391ca7b024ff26f0e35d51eaf5434124566f70be
peter.shlee <peter.shlee@kakaocorp.com>
2022-01-07 14:39:24 +0900

    flavor 생성

--- app/src/main/java/com/survivalcoding/todolist/di/DataSourceModule.kt
+++ app/src/main/java/com/survivalcoding/todolist/di/DataSourceModule.kt
@@ -1,18 +1,25 @@
 package com.survivalcoding.todolist.di
 
+import com.survivalcoding.todolist.BuildConfig
+import com.survivalcoding.todolist.data.datasource.ToDoInMemoryDataSource
 import com.survivalcoding.todolist.data.datasource.ToDoLocalDataSource
 import com.survivalcoding.todolist.data.datasource.ToDoRoomDataSource
-import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(ViewModelComponent::class)
-abstract class DataSourceModule {
+class DataSourceModule {
 
-    @Binds
-    abstract fun bindsToDoLocalDataSource(toDoRoomDataSource: ToDoRoomDataSource): ToDoLocalDataSource
+    @Provides
+    fun providesToDoLocalDataSource(
+        toDoRoomDataSource: ToDoRoomDataSource,
+        toDoInMemoryDataSource: ToDoInMemoryDataSource
+    ): ToDoLocalDataSource = if (BuildConfig.FLAVOR == "room") {
+        toDoRoomDataSource
+    } else {
+        toDoInMemoryDataSource
+    }
 }
