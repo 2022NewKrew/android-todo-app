f22706c0136fd9af7d9ee264dc40b530e833d043
Junsuk Oh <a811219@gmail.com>
2022-01-10 10:53:45 +0900

    쌩으로 DI (의존성 주입)

--- app/src/room/java/com/survivalcoding/todolist/App.kt
+++ app/src/room/java/com/survivalcoding/todolist/App.kt
@@ -7,12 +7,14 @@ import com.survivalcoding.todolist.data.repository.TodoRoomRepository
 import com.survivalcoding.todolist.domain.repository.TodoRepository
 
 class App : Application() {
+    private val todoDao by lazy {
+        Room.databaseBuilder(
+            this,
+            AppDatabase::class.java, "todo-db"
+        ).build().todoDao()
+    }
+
     val todoRepository : TodoRepository by lazy {
-        TodoRoomRepository(
-            Room.databaseBuilder(
-                this,
-                AppDatabase::class.java, "todo-db"
-            ).build().todoDao()
-        )
+        TodoRoomRepository(todoDao)
     }
 }
